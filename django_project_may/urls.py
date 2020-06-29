ccc23942f545afd1c36b7dcb6a158b9cb9ce0fec
Jake Chamblee <jchamblee1995@gmail.com>
2020-06-29 18:13:34 -0500

    Django_project_2 files

---
+++ django_project_may/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+# the include function 'chops off' the string up to the match, sending only the remainder of the string
+# along the path to the route designated in include() for further processing
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('', include('blog.urls')),
+    path('fitness/', include('fitness.urls')),
+    path('polls/', include('polls.urls')),
+    path('restapi/', include('restapi.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
