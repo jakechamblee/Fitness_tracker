cc425c49cbf5905aff92d7ca7c34decfd9cf0562
Jake Chamblee <jchamblee1995@gmail.com>
2020-07-03 23:40:51 -0500

    Django_project_2 files

--- fitness/tests.py
+++ fitness/tests.py
@@ -14,24 +14,22 @@ class TestFitnessResponses:
 
     @pytest.fixture(autouse=True)
     def setup_stuff(self, db):
-        # For some reason, could not access the database to create a user, despite being capable of creating other
-        # model objects like Exercise/ExerciseInstance. Needed to use this fixture setup as a workaround.
+        # For some reason, could not access the database to create a user, despite @pytest.mark.django_db
+        # being sufficient for model objects like Exercise/ExerciseInstance. This setup function is the workaround.
         global user
         user = User.objects.create_user(username=self.username, password=self.password)
         self.client.login(username=self.username, password=self.password)
+        Exercise.objects.create(name='Bicep Curl', type='db')
+        ExerciseInstance.objects.create(name=Exercise.objects.all()[0], sets=5, reps=5, weight=5, user=user)
 
     def test_fitness_home(self):
         assert self.client.get(reverse('fitness-home')).status_code == 200
 
     def test_fitness_progress(self):
-        Exercise.objects.create(name='Bicep Curl', type='db')
-        ExerciseInstance.objects.create(name=Exercise.objects.all()[0], sets=5, reps=5, weight=5, user=user)
         assert self.client.get(reverse('fitness-progress', kwargs={'exercise_name': 'Bicep Curl'})).status_code == 200
 
     def test_fitness_create(self):
         assert self.client.get(reverse('fitness-create')).status_code == 200
 
     def test_fitness_update(self):
-        Exercise.objects.create(name='Bicep Curl', type='db')
-        ExerciseInstance.objects.create(name=Exercise.objects.all()[0], sets=5, reps=5, weight=5, user=user)
         assert self.client.get(reverse('fitness-update', kwargs={'pk': 1})).status_code == 200
