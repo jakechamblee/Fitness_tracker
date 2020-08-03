4b9b727fb9f9fe61fa879fc5500ac936f989c45f
Jake Chamblee <41979532+jakechamblee@users.noreply.github.com>
2020-08-02 22:42:49 -0500

    Update settings.py
    Added dummy secret key

--- django_project_may/settings.py
+++ django_project_may/settings.py
@@ -3,6 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = "m_4sgkzhiq2dqofwjm_%_uqbj0u3zsx#p)xkeha$o8fjr1=f$b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
